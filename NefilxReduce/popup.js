aad17d09d48b05cf9ec1620ef6c9893764b20ca3
Robin leigh leitner <106034240+roleigh1@users.noreply.github.com>
2023-04-24 13:53:13 +0200

    update
    now the  countdown close netflix if the countdown is 00:00,  When u visiting netflix the extension opens,

--- NefilxReduce/popup.js
+++ NefilxReduce/popup.js
@@ -1,3 +1,48 @@
-var input = document.querySelector("#input"); 
-var submitBtn = document.querySelector("#submit");
+let countdown;
+const timerDisplay = document.querySelector(".timer");
+const input = document.querySelector("#input");
+const submitButton = document.querySelector("#submit");
 
+function timer(seconds) {
+  clearInterval(countdown);
+
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      closeNetflix();
+      return;
+    }
+
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes < 10 ? '0' : ''}${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  timerDisplay.textContent = display;
+}
+
+function closeNetflix() {
+  chrome.tabs.query({ url: "*://www.netflix.com/*" }, function (tabs) {
+    if (tabs.length > 0) {
+      chrome.tabs.remove(tabs[0].id);
+    }
+  });
+}
+submitButton.addEventListener("click", function() {
+    const inputVal = parseFloat(input.value);
+    if(isNaN(inputVal)) {
+      return;
+    }
+  
+    const seconds = Math.round(inputVal * 60);
+    timer(seconds);
+  });
