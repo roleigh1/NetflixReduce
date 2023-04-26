96bad160827d70a5b1d6ed71dffa4ac034b1a09c
Robin leigh leitner <106034240+roleigh1@users.noreply.github.com>
2023-04-26 21:03:59 +0200

    updated content.js
    today i build the block when visting netflix. The next changes in the content.js gonna be : unblock netflix when starting the timer.

--- NefilxReduce/content.js
+++ NefilxReduce/content.js
@@ -1,3 +1,21 @@
-if (window.location.href.indexOf("https://www.netflix.com") === 0) {
-  console.log("test");
+const generateHTML = () => {
+  return `<div id="headline">
+    Select the time on the popup you wanna spend on netflix.
+  </div>`
 }
+const generateCSS = () => {
+  return `<style> 
+
+  #headline {
+    color: black;
+  }
+  </style> `
+}
+  if(window.location.hostname === "www.netflix.com") {
+    document.body.innerHTML = generateHTML();
+    document.head.innerHTML = generateCSS();
+  }
+
+
+console.log("content script says hello ")
+
