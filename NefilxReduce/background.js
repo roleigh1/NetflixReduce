b1636b59577b622a17d14becf425d8f5bc95f05c
Robin leigh leitner <106034240+roleigh1@users.noreply.github.com>
2023-05-01 16:20:17 +0200

    Update background
    I updated the background. whenever the popup sends a message to the background we gonna send a message to the content.js

--- NefilxReduce/background.js
+++ NefilxReduce/background.js
@@ -3,23 +3,24 @@
 chrome.webNavigation.onCommitted.addListener(function(details) {
   if (details.url.startsWith("https://www.netflix.com/")) {
      chrome.windows.create({url: chrome.runtime.getURL("popup.html"), type:"popup", width : 400, height: 600});
-     chrome.contentSettings.javascript.set({
-      primaryPattern: "*://*.netflix.com/*", 
-      setting: "block"
-    });
   }
 });
-  
+
+
+
  chrome.runtime.onInstalled.addListener(function() {
     console.log("in the background.")
  })
  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action == "executeFunction") {
-    myFunction();
+    messageToContent()
   }
 });
 
-// function to be executed
-function myFunction() {
-  console.log("test");
+// function to send a message to content script 
+function messageToContent() {
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {message: "Hallo von Hintergrundskript!"})
+});
+  
 }
