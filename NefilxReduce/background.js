8dfccf4f62e9f8479cef9e72233bc2bcbbcda46c
Robin leigh leitner <106034240+roleigh1@users.noreply.github.com>
2023-05-03 00:33:59 +0200

    Update background.js
    added ; passing message to content script and send the input value for the time to our backgroud script,

--- NefilxReduce/background.js
+++ NefilxReduce/background.js
@@ -5,22 +5,24 @@ chrome.webNavigation.onCommitted.addListener(function(details) {
      chrome.windows.create({url: chrome.runtime.getURL("popup.html"), type:"popup", width : 400, height: 600});
   }
 });
-
-
-
+// We log something in the console to see the background script works 
  chrome.runtime.onInstalled.addListener(function() {
     console.log("in the background.")
  })
- chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+ // message passing between popup script and background script 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  const timeMinutes = request.timeInMinutes;
+  console.log("Input-Value:", timeMinutes);
   if (request.action == "executeFunction") {
     messageToContent()
   }
 });
 
-// function to send a message to content script 
+
+// function to send a message passing to contentscript
 function messageToContent() {
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, {message: "Hallo von Hintergrundskript!"})
+chrome.tabs.query({active: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {message: "Hello from backgroundscript!"})
 });
   
 }
