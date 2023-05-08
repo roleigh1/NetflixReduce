ab54129974b167e0848fb22e9bb51474efdb233e
Robin leigh leitner <106034240+roleigh1@users.noreply.github.com>
2023-05-08 18:55:56 +0200

    Add files via upload

---
+++ NefilxReduce/content.js
@@ -0,0 +1,41 @@
+
+
+const generateHTML = () => {
+  return `<div id="headline">
+    Select the time on the popup you wanna spend on netflix.
+  </div>`
+}
+const generateCSS = () => {
+  return `<style> 
+  body {
+    background-color: grey; 
+   
+  }
+  #headline {
+    color: black;
+  }
+  </style> `
+}
+let originalHTML; 
+let originalCSS; 
+
+  if(window.location.hostname === "www.netflix.com") {
+    originalHTML = document.body.innerHTML; 
+    originalCSS = document.head.innerHTML; 
+
+    document.body.innerHTML = generateHTML();
+    document.head.innerHTML = generateCSS()
+  }
+  if(window.location.pathname === "watch/*"){
+  document.body.innerHTML = originalHTML; 
+  document.head.innerHTML = originalCSS;
+   
+  }
+ // message passing from background script 
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if(request.message === "Hello from backgroundscript!"){
+      console.log(request.message);
+      document.body.innerHTML = originalHTML;
+      document.head.innerHTML = originalCSS; 
+    }
+  })
