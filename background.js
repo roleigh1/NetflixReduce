8d2fd8f4e005bde899691c73227a7665119609de
Robin leigh leitner <106034240+roleigh1@users.noreply.github.com>
2023-05-11 09:56:08 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,62 +1,71 @@
-let timerRunning = false; 
-// we open the html file when we visting netflix.
+
+var timerRunning = false;
+
+// we open the html file when we visit netflix.
 chrome.webNavigation.onCommitted.addListener(function(details) {
   if (details.url.startsWith("https://www.netflix.com/") && !timerRunning) {
-     chrome.windows.create({url: chrome.runtime.getURL("popup.html"), type:"popup", width : 400, height: 600});
+    chrome.windows.create({url: chrome.runtime.getURL("popup.html"), type:"popup", width : 400, height: 600});
     timerRunning = true; 
+    chrome.tabs.query({active: true}, function(tabs){ // message to contentscript to block netflix till timer is running
+    chrome.tabs.sendMessage(tabs[0].id, {message:"Block content!"});
+  })
   }
 });
-// We log something in the console to see the background script works 
- chrome.runtime.onInstalled.addListener(function() {
-    console.log("in the background.")
- })
- // message passing between popup script and background script 
+
+// We log something in the console to see the background script works
+chrome.runtime.onInstalled.addListener(function() {
+  console.log("in the background.");
+});
+
+// message passing between popup script and background script
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   var timeMinutes = request.timeInMinutes;
   console.log("Input-Value:", timeMinutes);
   if (request.action == "executeFunction") {
-    messageToContent()
+    messageToContent();
     startTimer(request.timeInMinutes);
-  } 
+  }
 });
-
 
 // function to send a message passing to contentscript
 function messageToContent() {
-chrome.tabs.query({active: true}, function(tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, {message: "Hello from backgroundscript!"})
-});
+  chrome.tabs.query({active: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {message: "Hello from backgroundscript!"});
+  });
 }
 
 // logic timer
-var timerId = 0; 
-var timerLeft = 0; 
+var timerId = 0;
+var timerLeft = 0;
 
 function startTimer(minutes) {
-  timerLeft = minutes *60;
-  timerId = setInterval(updateTimer,1000); 
+  timerLeft = minutes * 60;
+  timerId = setInterval(updateTimer, 1000);
 }
+
 function updateTimer() {
   timerLeft -= 1;
   if(timerLeft <= 0) {
     clearInterval(timerId);
-    chrome.runtime.sendMessage({timerFinished: true}); 
+    chrome.runtime.sendMessage({timerFinished: true});
     console.log("Timer abgelaufen!");
-    timerRunning = false; 
+    timerRunning = false;
   } else {
     chrome.runtime.sendMessage({timerValue: formatTime(timerLeft)});
     console.log("Verbleibende Zeit:" + formatTime(timerLeft));
   }
 }
+
 function formatTime(timeInSeconds) {
- var minutes = Math.floor(timeInSeconds / 60); 
- var seconds = timeInSeconds % 60; 
- return padZero(minutes) + ":" + padZero(seconds);
+  var minutes = Math.floor(timeInSeconds / 60);
+  var seconds = timeInSeconds % 60;
+  return padZero(minutes) + ":" + padZero(seconds);
 };
+
 function padZero(num) {
   if(num < 10) {
-    return "0" + num; 
+    return "0" + num;
   } else {
-    return "" + num; 
+    return "" + num;
   }
 }
