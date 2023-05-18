e30c77e570212acca636ff2c050fbf34be107ab1
Robin leigh leitner <106034240+roleigh1@users.noreply.github.com>
2023-05-18 21:25:21 +0200

    Update content.js
    added a background till a timer is set on the netflix page.

--- content.js
+++ content.js
@@ -1,25 +1,51 @@
 
 var generateHTML = () => {
-  return `<div id="headline">
-    Select the time on the popup you wanna spend on Netflix.
-  </div>`
+  return `
+  <div class="container">
+  <div id="cloud-intro"></div>
+</div>
+  `
 };
 
 var generateCSS = () => {
   return `<style> 
-    body {
-      background-color: grey; 
+  html, body, .container{
+    width: 100%;
+    height: 100%;
+    min-width: 500px;
+    min-height: 500px;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+  .container{
+    background-color: #007ced;
+    background: linear-gradient(to right, #261f21, #b20a2c);
+  }
+  #cloud-intro{
+    position: relative;
+    height: 100%;
+    background: url(https://static.radulescu.me/examples/clouds/clouds1000.png);
+    background: url(https://static.radulescu.me/examples/clouds/clouds1000.png) 0 200px,
+                url(https://static.radulescu.me/examples/clouds/clouds1200_1.png) 0 300px,
+                url(https://static.radulescu.me/examples/clouds/clouds1000_blur3.png) 100px 250px;
+    animation: wind 20s linear infinite;
+  }
+  @keyframes wind{
+    0% {
+      background-position: 0 200px, 0 300px, 100px 250px;
     }
-    #headline {
-      color: black;
+    100% {
+      background-position: 1000px 200px, 1200px 300px, 1100px 250px;
     }
+  
+  }
   </style>`
 };
 var originalHTML = document.body.innerHTML;
 var originalCSS = document.head.innerHTML;
 
 if (window.location.hostname === "www.netflix.com" && window.location.pathname === "/browse") {
-  // Code, der ausgeführt wird, wenn Sie sich auf "www.netflix.com/browse" befinden
 document.body.innerHTML = generateHTML();
 document.head.innerHTML = generateCSS(); 
 
@@ -33,4 +59,3 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     document.head.innerHTML = originalCSS;
   }
 });
-
